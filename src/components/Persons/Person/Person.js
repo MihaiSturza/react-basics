ea525716a87861dbdbfb75d16872b8821293a5f9
Mihai Sturza <sturzamimi@gmail.com>
2021-04-28 18:42:07 +0300

    testing github commit

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -38,12 +38,12 @@ class Person extends Component {
 
     return (
       <div className={classes.Person}>
-        <p onClick={ this.props.click }>I'm { this.props.name } and I'm { this.props.age } years old</p>
+        <p onClick={this.props.click}>My name is {this.props.name} and I'm {this.props.age} years old</p>
         <p>{this.props.children}</p>
-        <input type="text"  onChange={ this.props.changed } value={ this.props.name } />
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
       </div>
     );
   }
-} 
+}
 
 export default Person;
